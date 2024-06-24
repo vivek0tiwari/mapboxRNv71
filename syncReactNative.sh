3207d426db986362084cdad7aa533e0f0a2c007a
vivek tiwari <vivek.tiwari@aidash.com>
2024-06-24 18:45:31 +0530

    fix

---
+++ syncReactNative.sh
@@ -0,0 +1,3 @@
+rsync -avzh --delete --progress . ./../isms-ios/node_modules/mapboxRNv71 --exclude .git --exclude .babelrc --exclude node_modules
+
+rsync -avzh --delete --progress . ./../isms-android/node_modules/mapboxRNv71 --exclude .git --exclude .babelrc --exclude node_modules
